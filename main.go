69dbadc3b8ed3fbdda4bad757c75f78f34bf472a
Sid <sitaulasiddhartha2002@gmail.com>
2024-02-03 00:02:47 +0545

    [ADD] added put request

--- main.go
+++ main.go
@@ -46,9 +46,40 @@ func ListRecipesHandler(c *gin.Context) {
 	c.JSON(200, recipies)
 }
 
+func UpdateRecipeHandler(c *gin.Context) {
+	id := c.Param("id")
+	var recipe Recipe
+
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	index := -1
+
+	for i := 0; i < len(recipies); i++ {
+		if recipies[i].ID == id {
+			index = i
+		}
+	}
+
+	if index == -1 {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Recipe not found",
+		})
+	}
+
+	recipies[index] = recipe
+	c.JSON(http.StatusOK, recipe)
+}
+
 func main() {
 	router := gin.Default()
 	router.POST("/recipes", NewRecipeHandler)
 	router.GET("/recipes", ListRecipesHandler)
+	router.PUT("/recipes/:id", UpdateRecipeHandler)
+
 	router.Run(":8080")
 }
