9cbf1cf491732cb8638ab9adf44755ae314bf966
Sid <sitaulasiddhartha2002@gmail.com>
2024-02-02 23:51:48 +0545

    [ADD] added fetch all recipie

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"encoding/json"
+	"net/http"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
 )
 
 type Recipe struct {
+	ID           string    `json:"id"`
 	Name         string    `json:"name"`
 	Tags         []string  `json:"tags"`
 	Ingredients  []string  `json:"ingredients"`
@@ -14,8 +19,36 @@ type Recipe struct {
 	PublishedAt  time.Time `json:"publishedAt"`
 }
 
+var recipies []Recipe
+
+func init() {
+	recipies = make([]Recipe, 0)
+	file, _ := os.ReadFile("recipes.json")
+	_ = json.Unmarshal([]byte(file), &recipies)
+}
+
+func NewRecipeHandler(c *gin.Context) {
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	recipe.ID = xid.New().String()
+	recipe.PublishedAt = time.Now()
+	recipies = append(recipies, recipe)
+	c.JSON(http.StatusOK, recipe)
+}
+
+func ListRecipesHandler(c *gin.Context) {
+	c.JSON(200, recipies)
+}
+
 func main() {
 	router := gin.Default()
-
-	router.Run(":5000")
+	router.POST("/recipes", NewRecipeHandler)
+	router.GET("/recipes", ListRecipesHandler)
+	router.Run(":8080")
 }
