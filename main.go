7b008296a3077496716ac194f7d306a33bb23da6
Sid <sitaulasiddhartha2002@gmail.com>
2024-02-02 23:01:33 +0545

    [ADD] added recipe model

--- main.go
+++ main.go
@@ -1,43 +1,21 @@
 package main
 
 import (
-	"encoding/xml"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-type Person struct {
-	XMLName  xml.Name `xml:"user"`
-	UserName string   `xml:"uname,attr"`
-	Age      int32    `xml:"age,attr"`
-}
-
-func IndexHandler(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"message": "Hello world",
-	})
-}
-
-func UserHandler(c *gin.Context) {
-	name := c.Params.ByName("name")
-	c.JSON(200, gin.H{
-		"message": "Hello " + name,
-	})
-}
-
-func UserHandlerXML(c *gin.Context) {
-	c.XML(200, Person{
-		UserName: "sid",
-		Age:      22,
-	})
+type Recipe struct {
+	Name         string    `json:"name"`
+	Tags         []string  `json:"tags"`
+	Ingredients  []string  `json:"ingredients"`
+	Instructions []string  `json:"instructions"`
+	PublishedAt  time.Time `json:"publishedAt"`
 }
 
 func main() {
 	router := gin.Default()
-
-	router.GET("/", IndexHandler)
-	router.GET("/:name", UserHandler)
-	router.GET("/user", UserHandlerXML)
 
 	router.Run(":5000")
 }
